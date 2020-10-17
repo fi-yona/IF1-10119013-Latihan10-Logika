2bb8ca3482c45551929494d0e86b1cc798aea51e
FionaAvila <fionaavila15@gmail.com>
2020-10-17 14:00:17 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan10/logika/if110119013Latihan10Logika.java
@@ -0,0 +1,28 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package if1.pkg10119013.latihan10.logika;
+
+/**
+ *
+ * @author Fiona Avila
+ */
+
+public class if110119013Latihan10Logika {
+
+    public static void main(String[] args) {
+        
+        int umur = 19;
+        boolean dewasa;
+        
+        dewasa = (umur >= 17);
+        
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
